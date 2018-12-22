9475f38756efa355b12643b3d476ede3eff05b93
Md Jahidul Islam <xahid@cs-stargazer-umh.cs.umn.edu>
2018-12-21 19:01:44 -0600

    unit test

--- rgb_lab_formulation.py
+++ rgb_lab_formulation.py
@@ -1,25 +1,25 @@
-'''
-Functions for
-      RGB to LAB color-space conversion
-      LAB to RGB color-space conversion
-Use the test_.py for testing
-	  --Md Jahidul Islam (islam034@umn.edu)
+#!/usr/bin/env python
+"""
+Maintainer: Jahid (email: islam034@umn.edu)
+Interactive Robotics and Vision Lab
+http://irvlab.cs.umn.edu/
+Any part of this repo can be used for academic and educational purposes only
 
-MASSIVE help from:
-------------------
+Acknowledgement-
+Some modules are collected from:
+--------------------------------
 https://github.com/affinelayer/pix2pix-tensorflow/blob/master/pix2pix.py
 https://github.com/torch/image/blob/9f65c30167b2048ecbe8b7befdc6b2d6d12baee9/generic/image.c
 https://github.com/cameronfabbri/Colorizing-Images-Using-Adversarial-Networks
-'''
+"""
 
-from __future__ import division
-from __future__ import absolute_import
-
+from __future__ import division # dont remove this
 import tensorflow as tf
-import math
-import random
-import glob
-import fnmatch
+"""
+modules for
+      RGB to LAB color-space conversion
+      LAB to RGB color-space conversion
+"""
 
 
 def preprocess(image):
